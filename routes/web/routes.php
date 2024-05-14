dbeed52b92531d0f0f5edda7ff8ed70632976f67
andrea <andrea.zollini@proton.me>
2024-05-14 14:16:13 +0200

    fix routes file

--- routes/web/routes.php
+++ routes/web/routes.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::as('pages')->group(function () {
-    Route::get('/', function () {
-        return view('pages.index');
-    })->name('home');
+Route::as('pages:')->group(function () {
+    Route::view('/', 'pages.index')->name('home');
 });
